1d9880f76f44d13a808fc33f2efe66ec0c454b4f
Jonathan Ramanantsoa <jonathan.rama2005@gmail.com>
2021-07-28 16:28:41 +0800

    too slow

--- Niveau 3/9 - Éfficacité temporelle/collage_daffiches.cpp
+++ Niveau 3/9 - Éfficacité temporelle/collage_daffiches.cpp
@@ -1,47 +1,34 @@
+#include <forward_list>
 #include <iostream>
-#include <list>
-size_t height;
-char query;
-size_t size;
 
-int main()
-{
+int main() {
     size_t nbRequetes;
     std::cin >> nbRequetes;
-    std::list<size_t> stack{};
-    for (size_t i = 0; i < nbRequetes; i++)
-    {
-        std::cin >> query;
-        if (query == 'Q')
-        {
-            if (stack.empty())
-            {
-                size = 0;
-            }
-            else
-            {
-                size = 1;
-                size_t max = stack.front();
+    std::forward_list<size_t> stack{};
+    char query;
+    size_t height;
+    size_t size = 0;
 
-                for (auto i = ++stack.begin(); i != stack.end(); i++)
-                {
-                    if (*i > *stack.begin() && *i > max)
-                    {
-                        size++;
-                        max = *i;
-                    }
-                    else
-                    {
-                        stack.erase(i);
-                    }
-                }
-            }
+    for (size_t i = 0; i < nbRequetes; i++) {
+        std::cin >> query;
+
+        if (query == 'Q') {
             std::cout << size << '\n';
-        }
-        else if (query == 'C')
-        {
+        } else if (query == 'C') {
             std::cin >> height;
+
+            stack.remove_if([height, &size](size_t n) {
+                if (n <= height) {
+                    size--;
+                    return true;
+                }
+                return false;
+            });
+
             stack.push_front(height);
+            size++;
         }
     }
+
+    return 0;
 }
