fc309829f7233bc4432b3eae87d6525f7c0c920b
Jonathan Ramanantsoa <jonathan.rama2005@gmail.com>
2021-07-30 11:11:02 +0800

    too slow

--- Niveau 3/9 - Éfficacité temporelle/collage_daffiches.cpp
+++ Niveau 3/9 - Éfficacité temporelle/collage_daffiches.cpp
@@ -1,6 +1,5 @@
 #include <forward_list>
 #include <iostream>
-
 int main() {
     size_t nbRequetes;
     std::cin >> nbRequetes;
@@ -8,15 +7,12 @@ int main() {
     char query;
     size_t height;
     size_t size = 0;
-
     for (size_t i = 0; i < nbRequetes; i++) {
         std::cin >> query;
-
         if (query == 'Q') {
             std::cout << size << '\n';
         } else if (query == 'C') {
             std::cin >> height;
-
             stack.remove_if([height, &size](size_t n) {
                 if (n <= height) {
                     size--;
@@ -24,11 +20,9 @@ int main() {
                 }
                 return false;
             });
-
             stack.push_front(height);
             size++;
         }
     }
-
     return 0;
 }
